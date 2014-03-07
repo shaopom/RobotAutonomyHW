d5d15434f259169e01e26b0e1aadf61570c4b348
Denis Anisimov <dbanisimov@gmail.com>
2014-03-07 18:07:12 -0500

    Simple environment random conf generation: 1) Added default value 2) Moved random generation out of the loop

--- hw2/SimpleEnvironment.py
+++ hw2/SimpleEnvironment.py
@@ -2,7 +2,7 @@ import numpy
 import pylab as pl
 
 class SimpleEnvironment(object):
-    
+
     def __init__(self, herb):
         self.robot = herb.robot
         self.boundary_limits = [[-5., -5.], [5., 5.]]
@@ -11,9 +11,9 @@ class SimpleEnvironment(object):
         table = self.robot.GetEnv().ReadKinBodyXMLFile('models/objects/table.kinbody.xml')
         self.robot.GetEnv().Add(table)
 
-        table_pose = numpy.array([[ 0, 0, -1, 1.0], 
-                                  [-1, 0,  0, 0], 
-                                  [ 0, 1,  0, 0], 
+        table_pose = numpy.array([[ 0, 0, -1, 1.0],
+                                  [-1, 0,  0, 0],
+                                  [ 0, 1,  0, 0],
                                   [ 0, 0,  0, 1]])
         table.SetTransform(table_pose)
 
@@ -23,21 +23,21 @@ class SimpleEnvironment(object):
     def SetGoalParameters(self, goal_config, p = 0.2):
         self.goal_config = goal_config
         self.p = p
-        
+
     def GenerateRandomConfiguration(self):
         config = [0] * 2;
         lower_limits, upper_limits = self.boundary_limits
-        #
-        # TODO: Generate and return a random configuration
-        #
+
+        # Save robot current transform
+        init_transform = self.robot.GetTransform()
+        res = init_transform[:2, 3]
 
         # try at most 100 times to find one random collision free config
+        xs = numpy.random.uniform(lower_limits[0], upper_limits[0], 100)
+        ys = numpy.random.uniform(lower_limits[1], upper_limits[1], 100)
         for n in xrange(100):
-            
             # get uniform sampling in 2D configuration space
-            x = numpy.random.uniform(lower_limits[0], upper_limits[0])
-            y = numpy.random.uniform(lower_limits[1], upper_limits[1])
-            config = [x, y]
+            config = [xs[n], ys[n]]
 
             # transform robot to that config in 6D (x,y,z,r,p,y) space
             # only set the x and y in that 4 by 4 transform matrix
@@ -47,10 +47,15 @@ class SimpleEnvironment(object):
 
             # if this 2D random config sampling is collision free, then return this 2D config
             if not self.robot.GetEnv().CheckCollision(self.robot):
-                return numpy.array(config)
-        
-        # +++++ need to return some error +++++ please make a decision!
-        return numpy.array(config)
+                res = numpy.array(config)
+                break
+
+        # Restore robot transform
+        self.robot.SetTransform(init_transform)
+
+        # Return found random configuration
+        # If no collision-free configuration found, then return robots position
+        return res
 
     def ComputeDistance(self, start_config, end_config):
         #
@@ -62,18 +67,18 @@ class SimpleEnvironment(object):
         return numpy.linalg.norm(econfig - sconfig)
 
     def Extend(self, start_config, end_config):
-        
+
         #
-        # TODO: Implement a function which attempts to extend from 
+        # TODO: Implement a function which attempts to extend from
         #   a start configuration to a goal configuration
         #
         pass
 
     def ShortenPath(self, path, timeout=5.0):
-        
-        # 
+
+        #
         # TODO: Implement a function which performs path shortening
-        #  on the given path.  Terminate the shortening after the 
+        #  on the given path.  Terminate the shortening after the
         #  given timout (in seconds).
         #
         return path
@@ -101,11 +106,11 @@ class SimpleEnvironment(object):
                      bb.pos()[1] + bb.extents()[1],
                      bb.pos()[1] + bb.extents()[1],
                      bb.pos()[1] - bb.extents()[1]], 'r')
-                    
-                     
+
+
         pl.ion()
         pl.show()
-        
+
     def PlotEdge(self, sconfig, econfig):
         pl.plot([sconfig[0], econfig[0]],
                 [sconfig[1], econfig[1]],
